f9f30c1eb7905be943f14fc327aa8e2c996db763
creater toro <bablupandey446@gmil.com>
2024-09-20 03:51:43 +0530

    The sum of two number

---
+++ input_and_operators/SumOfTwoNumber.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+ int main (){
+
+  int x,y ;
+  cout<<"Enter 1st number is :";
+  cin>>x;
+  cout<<"Enter 2nd number is :";
+  cin>>y;
+  cout<<"sum of the numbers two number "<<x+y<<endl;
+ }
