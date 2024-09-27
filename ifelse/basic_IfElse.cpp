ce3f472f6e3f65c424153da9b0c63436b13c3190
creater toro <bablupandey446@gmil.com>
2024-09-27 21:15:59 +0530

    ifelse

---
+++ ifelse/basic_IfElse.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"Enter a number :"<<endl;
+int n;
+cin>>n;
+if(n%2==0){
+
+cout<<"Even";
+
+}
+
+else{
+     cout<<"odd";
+}
+
+return 0;
+   
+}
