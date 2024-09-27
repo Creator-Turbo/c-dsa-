9cab53b40d304132a3981c49888cc507037fe002
creater toro <bablupandey446@gmil.com>
2024-09-27 21:31:48 +0530

    Divisible by 5 or not

---
+++ ifelse/dibisbleby5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main (){
+
+cout<<"Enter a number :"<<endl;
+int n;
+cin>>n;
+if(n%5==0){
+  cout<<"Divisble by 5";
+}
+
+else{
+  cout<<"Not Divisble by 5";
+
+}
+
+return 0;
+}
