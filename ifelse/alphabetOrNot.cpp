258fe3ca5625459a29186a77763ca225c7d46847
creater toro <bablupandey446@gmil.com>
2024-10-03 04:05:08 +0530

    alphabetORrNot

---
+++ ifelse/alphabetOrNot.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+  char ch;
+  cout<<"Enter a character :";
+  cin>>ch;
+  // a to z --> 97 to 122
+  // A to z ---> 65 to 90
+   int ascii = (int)ch;
+    if(ascii>=97 && ascii<=122){
+      cout<<"The character is lowercase alphabet";
+    }
+    if(ascii>=64 && ascii<=90){
+     cout<<"The character is uppercase alphabet";
+    }
+    }
