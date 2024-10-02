557b8571c3d1eece1efebc9be65387dcb36e4382
creater toro <bablupandey446@gmil.com>
2024-10-03 05:04:06 +0530

    5 and 3 nested

---
+++ ifelse/5and3nested.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cout<<"Enter a number :";
+  cin>>n;
+  if(n%5==0){
+    if(n%3==0){
+     cout<<"Number is divisble by 5 and 3 both";
+
+    }
+  else{
+  cout<<"Number is divisble by 5 but not 3";
+  }
+  }
+  else{
+   cout<<"not maching conditon";
+  }
+  }
