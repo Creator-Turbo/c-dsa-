f3c548ddb73f7c2c5d073fe9918c968bb3fd234e
creater toro <bablupandey446@gmil.com>
2024-10-04 03:47:10 +0530

    grestest of two number on assi

---
+++ C++ Assignment 1/grestestofthem.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a ;
+cout<<"Enter fist number:";
+cin>>a;
+int b ;
+cout<<"Enter second number:";
+cin>>b;
+if(a>b){
+cout<<"Fist number "<<a<<" is the largest";
+
+}
+
+else{
+
+cout<<"second number "<<b<<" is the largest";
+}
+}
