81b25fa206e031a6118ae44256431d6601a1176e
creater toro <bablupandey446@gmil.com>
2024-10-09 04:42:55 +0530

    loops assigment 1

---
+++ loops assignment 2/ap.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   int n;
+   cout<<"Enter a number :";
+   cin>>n;
+
+   int a = 1;
+
+   for(int i =4;i<=n; i++){
+       if(a==1){
+         
+       }
+       else {
+        cout<<a<<endl;
+       }
+   
+     a = a + 3;
+   }
+}
