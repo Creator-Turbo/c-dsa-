4f6ec33f900da1af92b0e59e8c7292f131f81641
creatortorbo <bablupandey446@gmail.com>
2025-01-09 05:04:33 +0530

    starBridge

--- pattern2/starBridge.cpp
+++ pattern2/starBridge.cpp
@@ -1,32 +1,39 @@
 #include<iostream>
 using namespace std;
-int main(){
+
+int main() {
     int n;
-    cout<<"No of lines :";
-    cin>>n;
-    // 2n-1 starts 
-    for(int i=1;i<=2*n-1;i++){
-        cout<<"*";
+    cout << "No of lines: ";
+    cin >> n;
+
+    // Top line: Print 2n-1 stars
+    for (int i = 1; i <= 2 * n - 1; i++) {
+        cout << "*";
     }
-    cout<<endl;
-    int m = n-1;  // new lines 
-    int nsp = 1;
-    for(int i=1;i<=m;i++){
-        // stars 
-        for(int j=1;j<=m+1;j++){
-            cout<<"*";
+    cout << endl;
+
+    int nsp = 1;  // Initial spaces in the second line
+
+    // Loop for subsequent lines
+    for (int i = 1; i <= n - 1; i++) {
+        // Print decreasing stars on the left
+        for (int j = 1; j <= n - i; j++) {
+            cout << "*";
         }
 
-        // spaces 
-        for(int k=1;k<=nsp;k++){
-            cout<<" ";
+        // Print increasing spaces in the middle
+        for (int k = 1; k <= nsp; k++) {
+            cout << " ";
         }
-        nsp+=2;
-        // stars
-        for(int j=1;j<m+1;j++){
-            cout<<"*";
-        } 
-        cout<<endl;
-    
+        nsp += 2;
+
+        // Print decreasing stars on the right
+        for (int j = 1; j <= n - i; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
     }
+
+    return 0;
 }
