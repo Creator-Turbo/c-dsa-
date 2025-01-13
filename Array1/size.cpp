21b8475c5e60290eca7426060cd4ae8704dc93a5
Creator-Turbo <bablupandey446@gmail.com>
2025-01-14 05:23:19 +0530

    size

---
+++ Array1/size.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {1,2,3,4,5,89,0,3,2,1,2,3,4,4,5,5,6,6,7,7,7,87};
+    int size = sizeof(arr)/sizeof(arr[0]);
+     cout<<size;
+}
