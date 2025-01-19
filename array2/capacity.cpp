e7887dafba4e8937db1d737a90f9c22c63170e81
Creator-Turbo <bablupandey446@gmail.com>
2025-01-20 03:58:06 +0530

    capacity

---
+++ array2/capacity.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> v;
+    v.push_back(6);
+    v.push_back(7);
+    v.push_back(8);
+    v.push_back(9);
+    v.push_back(10);
+    v.push_back(1);
+    v.push_back(0);
+    v.push_back(6);
+    v.push_back(1);
+    v.push_back(6);
+    v.push_back(7);
+    v.push_back(8);
+    v.push_back(9);
+    v.push_back(10);
+    v.push_back(1);
+    v.push_back(0);
+    v.push_back(6);
+    v.push_back(1);
+
+    cout<<"size is :"<<v.size()<<endl;
+    cout<<"capacity is :"<<v.capacity()<<endl;
+
+    v.pop_back();
+    v.pop_back();
+    v.pop_back();
+    v.pop_back();
+    v.pop_back();
+    v.pop_back();
+    v.pop_back();
+    v.pop_back();
+    v.pop_back();
+
+    cout<<"size is :"<<v.size()<<endl;
+    cout<<"capacity is :"<<v.capacity()<<endl;
+}
